289882066328a6bc4cb7e6b7febb94a9bc6bed63
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-06-24 02:18:41 -0300

    Adiciona botões de material complementar e wallpapers

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,4 @@
-import { DiscordLogo, Lightning } from "phosphor-react"
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react"
 
 export const Video = () => {
   return (
@@ -44,6 +44,40 @@ export const Video = () => {
               Acesse o desafio
             </a>
           </div>
+        </div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40}/>
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">
+                Material complementar
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24}/>
+            </div>
+          </a>
+          <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40}/>
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">
+                Wallpapers exclusivos
+              </strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize sua máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24}/>
+            </div>
+          </a>
         </div>
         <div>
         </div>
